4e1de303afa82e310178c9f71de8e4d536943f41
SanFabryino <31211801+SanFabryino@users.noreply.github.com>
2019-09-25 16:13:56 +0200

    fixed bugs and added new features

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,21 @@
 
 //jsonph
 
-const url = 'https://jsonplaceholder.typicode.com/todos?_limit=3';
+const url = 'https://jsonplaceholder.typicode.com/todos?_limit=9';
 
 async function getDataFromCinemasAPI() {
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
+  
+  const cardContentForFilling = '<div class="col-md-4"><div class="card mb-4 box-shadow"><div class="card-body"><p class="card-text"> <p class="price"></p></p></div></div></div>';
 
-    for (let i = 0; i < data.length; i++) {
-        $('.card-text.c1').html((data[i]["title"]));
-        $('.card-text.c2').html((data[i]["title"]));
-        $('.card-text.c3').html((data[i]["title"]));
-    }    
+  for (let i = 0; i < data.length; i++) {
+    $('.append-here').append(cardContentForFilling);
+    let cards = $('.card-text');
+    let cardsPrices = $('.price');
+    cards.eq(i).html((data[i]["title"]));
+    cardsPrices.eq(i).html('€' + (data[i]["id"]));
+  }
 }
 
 getDataFromCinemasAPI();
